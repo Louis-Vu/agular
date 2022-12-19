4d1e58b6d1561ad683589cfeff1ea861a5fb2733
Louis-Vu <hoangvuhuy04@gmail.com>
2022-12-19 09:37:40 +0700

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import {studentComponent} from "./students/student.component";
+import {HomeComponent} from "./pages/Home/home.component";
+import {RouterModule, Routes} from "@angular/router";
+import {CategoryComponent} from "./pages/Category/Category.Component";
+import {RegisterComponent} from "./pages/Register/Register.Component";
+import {ProductComponent} from "./pages/Product/Product.Component";
+import {LoginComponent} from "./pages/Login/Login.Component";
+import {HttpClientModule} from "@angular/common/http";
+//1. khai bao danh sach cac routing
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'category', component: CategoryComponent},
+  { path: 'product/:id', component: ProductComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,studentComponent,
+    HomeComponent,CategoryComponent,RegisterComponent,ProductComponent,LoginComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
